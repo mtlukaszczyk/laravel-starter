8a0ef1019d166d18b727e80d9ffd7e8534540053
Michał Łukaszczyk <mtlukaszczyk@gmail.com>
2024-11-13 09:28:43 +0100

    Update SendMail.php

--- app/Jobs/SendMail.php
+++ app/Jobs/SendMail.php
@@ -13,16 +13,16 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Contracts\Translation\HasLocalePreference;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Contracts\Mail\Mailer;
+use Illuminate\Queue\Attributes\DeleteWhenMissingModels;
+use Illuminate\Queue\Attributes\WithoutRelations;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
+#[WithoutRelations]
+#[DeleteWhenMissingModels]
 class SendMail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
-    public bool $deleteWhenMissingModels = true;
-
-    private Mailer $mailer|false = false;
 
     public function __construct(
         protected Mailable $mailable,
@@ -35,15 +35,11 @@ class SendMail implements ShouldQueue
         $this->onQueue('default');
     }
 
-    public function handle(): void
+    public function handle(Mailer $mailer): void
     {
-        if (!$this->mailer) {
-            $this->mailer = app(Mailer::class);
-        }
-        
         try {
             event(new Created($this->mailable, $this->recipient));
-            $this->mailer->to($this->recipient->getEmail())->send($this->mailable);
+            $mailer->to($this->recipient->getEmail())->send($this->mailable);
             event(new Sent($this->mailable, $this->recipient));
         } catch (Exception $e) {
             throw new MailException($this->mailable, $this->recipient, $e->getMessage());
